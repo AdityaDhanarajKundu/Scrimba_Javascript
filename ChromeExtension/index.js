651f7ff47fa9a4c62895f449d7b58e1d8c11cd60
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-07-28 10:15:28 +0530

    rendering the list items and clearing the input text field

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -5,12 +5,22 @@ const buttonEl=document.getElementById("input-btn");
 
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
-
+const ulEl = document.getElementById("ul-el");
 //log out the "Button Clicked" when the user clicks the button
 //Push the value "www.awesomelead.com" to the array when the button is clicked
 buttonEl.addEventListener("click", ()=>{
     //pushing the value in the text field into the array
     myLeads.push(inputEl.value);
-    console.log(myLeads);
-    alert("Lead added to the array");
+    renderLeads();
+    //clearing the value of the textfield after the button is clicked
+    inputEl.value="";
 });
+
+let listItems = "";
+
+function renderLeads(){
+    for(let i=0;i<myLeads.length;i++){
+        listItems+="<li>"+myLeads[i]+"</li>";
+    }
+    ulEl.innerHTML=listItems;
+}
