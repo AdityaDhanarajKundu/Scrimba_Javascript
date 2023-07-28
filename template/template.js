5dfd4dec34ac6c0e65ce142fe80e2679aef8cc3f
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-07-28 12:16:45 +0530

    diff between the double quote and the backticks multiline/// used an sender variable for sender name

--- template/template.js
+++ template/template.js
@@ -2,7 +2,16 @@
 
 const recipient = "James"
 
+//create a sender variable and assign it your name and refactor it into the email
+const sender="Aditya Kundu"
+
 // Refactor the email string to use template strings
-const email = `Hey ${recipient} ! How is it going? Cheers Per`;
+//Break the email string into multiple lines
+const email = ` Hey ${recipient} !
+
+ How is it going? 
+
+ Cheers ${sender}
+ `;
 
 console.log(email)
