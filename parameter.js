24ccd8326b132ce239d23321bab75578a1ab386f
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-07-31 00:01:14 +0530

    updated the js file

--- parameter.js
+++ parameter.js
@@ -5,3 +5,15 @@ function greetUser(name) {
 }
 
 greetUser("Aditya Kundu");
+
+//create a function, getFirst(arr), that returns the first item in the array
+
+function getFirst(arr){
+    return arr[0];
+}
+
+let myArr=[1,2,3,4];
+
+//Call it with an array as an argument to verify that it works
+let element = getFirst(myArr);
+console.log(element);
